f89bcc67d72a757df614176e6bf08ffeb90fce1f
pkapadia9871 <30789273+pkapadia9871@users.noreply.github.com>
2023-11-07 15:29:58 -0600

    Add files via upload

---
+++ Ex_1_2/recipe.py
@@ -0,0 +1,6 @@
+recipe_1 = {'name': "Tea", 'cooking_time ': 5, 'ingredients ': ['Tea leaves', 'Sugar', 'Water']}
+recipe_2 = {'name': "Tea", 'cooking_time ': 5, 'ingredients ': ['Tea leaves', 'Sugar', 'Water']}
+recipe_3 = {'name': "Tea", 'cooking_time ': 5, 'ingredients ': ['Tea leaves', 'Sugar', 'Water']}
+recipe_4 = {'name': "Tea", 'cooking_time ': 5, 'ingredients ': ['Tea leaves', 'Sugar', 'Water']}
+recipe_5 = {'name': "Tea", 'cooking_time ': 5, 'ingredients ': ['Tea leaves', 'Sugar', 'Water']}
+all_recipes = [recipe_1,recipe_2,recipe_3,recipe_4,recipe_5]
